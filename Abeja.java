78e73e5cb7e46a7f3505c2c525c6f0b346b5f80a
cmorenoruiz <cmorenoruiz@educa.madrid.org>
2023-03-06 17:59:46 +0100

    Añadido el método void muestra() a las abejas y la creación de unpar de abjejas de prueba en el main.

--- Abeja.java
+++ Abeja.java
@@ -10,20 +10,32 @@ package bees;
  */
 public abstract class Abeja {
 
-    protected enum Sexo {FEMENINO, MASCULINO};
-    protected enum Tipo {OBRERA,ZANGANO,REINA};
+    protected enum Sexo {
+        FEMENINO, MASCULINO
+    };
 
-    Sexo sexo;
-    
+    protected enum Tipo {
+        OBRERA, ZANGANO, REINA
+    };
+
+    private Sexo sexo;
+
     public Abeja(Sexo sexo) {
-        this.sexo=sexo;
+        this.sexo = sexo;
     }
-    
-    public void vuela(){
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public abstract Tipo getTipo();
+
+    void muestra() {
+        System.out.println("Abeja de sexo " + getSexo() + " y de tipo " + getTipo());
+    }
+
+    public void vuela() {
         System.out.println("Soy una abeja volando...");
     }
-    
-    public abstract Tipo getTipo();
-    
-    
+
 }
--- Bees.java
+++ Bees.java
@@ -15,6 +15,14 @@ public class Bees {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        // Voy a crear tres abejas y a mostrarlas por pantalla
+        Abeja abeja1, abeja2;
+        //abeja1 = new Obrera();
+        abeja2 = new Zangano();
+        Abeja abeja3 = new Reina();
+        abeja2.muestra();
+        abeja3.muestra();
+
     }
-    
+
 }
